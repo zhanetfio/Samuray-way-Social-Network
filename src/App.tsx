ab9bfdf82db938ff854529e13b63bc74f82338df
Zhanet <zhanet_f@mail.ru>
2022-07-16 18:46:49 +0300

    feat: add App and module.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from "./components/Header/Header";
+import Navbar from "./components/Navbar/Navbar";
+import Profile from "./components/Profile/Profile";
+import Dialogs from "./components/Dialogs/Dialogs";
+import {Route, Routes} from "react-router-dom";
+import {RootStateType} from "./redux/state";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+type AppPropsType = {
+    appState: RootStateType
+}
+function App(props:AppPropsType) {
+
+
+    return (
+        <div className="app-wrapper">
+            <Header/>
+            <Navbar/>
+            <div className="app-wrapper-content">
+                <Routes>
+                    <Route path="/profile" element={<Profile postsState={props.appState.profilePage} />}/>
+                    <Route path="/dialogs" element={<Dialogs dialogs={props.appState.messagesPage} />}/>
+                </Routes>
+            </div>
+
+
+        </div>
+    );
 }
 
 export default App;
